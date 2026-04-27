36f607a97aabef894c9cfb7d96d17790548ba45d
Jnayna 🐾 Jinene Ben Said <101910279+Jinene@users.noreply.github.com>
2026-04-27 22:28:18 +0100

    Implement UDP client for sending and receiving messages

---
+++ tcp_udp/udp_client.py
@@ -0,0 +1,12 @@
+import socket
+
+HOST = '127.0.0.1'
+PORT = 6000
+
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+while True:
+    msg = input("You: ")
+    client.sendto(msg.encode(), (HOST, PORT))
+    data, _ = client.recvfrom(1024)
+    print(f"Server: {data.decode()}")
